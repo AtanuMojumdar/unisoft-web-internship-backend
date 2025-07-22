3a4c12d29035382dd583accb30e5cf34e8d1fe2d
Atanu Mojumdar <atanu192003mojumdar@gmail.com>
2025-07-22 17:48:22 +0530

    signup commit

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 const express = require("express")
 const dbConnection = require("./db/dbConnection.js")
 const cors = require("cors")
+const User = require("./db/models/UserSchema.js")
+const bcrypt = require("bcryptjs")
 
 //GLOBAL
 const app = express()
@@ -16,6 +18,44 @@ app.get("/", (req, res) => {
     return res.send("Hello World")
 })
 
+app.post("/signup", async (req, res) => {
+    try {
+        const body = req.body;
+
+        const user = await User.findOne({
+            email: body.email
+        })
+
+        if (user) {
+            return res.json({
+                message: "user already exists!"
+            })
+        }
+        else {
+            console.log("New User!")
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(body.password, salt);
+
+        const newUser = await User.create({
+            email: body.email,
+            password: hash,
+        })
+
+        
+
+
+        return res.json({
+            message: "success"
+        })
+    }
+    catch (error) {
+        console.log("Error Occured!");
+        console.log(error)
+    }
+
+})
 
 //MAIN
 async function main() {
